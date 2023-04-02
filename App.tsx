f891a45e08cfa91ce2d2e9ed4dc997f8ad69169a
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2023-04-02 12:39:19 -0300

    Create header component

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from 'styled-components'
 import theme from "./src/theme";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
 
@@ -14,6 +15,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,10 @@
-import { Container, } from "./styles";
+import { Header } from "@components/Header";
+import { Container } from "./styles";
 
 export function Groups() {
   return (
     <Container>
-
+      <Header />
     </Container>
   )
 }
