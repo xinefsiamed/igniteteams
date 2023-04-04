e1965c7c077d36ea541152a691e46703a2ca8557
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2023-04-04 18:11:04 -0300

    Creating listempty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,35 @@
+import { FlatList } from 'react-native'
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+
+  const [groups, setGroups] = useState<string[]>([
+  ])
+
+
   return (
     <Container>
       <Header showBackButton />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+
+        )
+        }
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
+      />
     </Container>
   )
 }
