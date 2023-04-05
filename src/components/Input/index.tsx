d9839b7ee580e2118b0cd5e5791ce1686ecce905
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2023-04-05 19:38:48 -0300

    Create input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,14 @@
+import { TextInputProps } from "react-native";
+import { Container } from "./styles";
+
+
+type InputProps = TextInputProps & {
+
+}
+
+export function Input({ ...rest }: InputProps) {
+
+  return (
+    <Container {...rest} />
+  )
+}
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@components/Header";
 import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -12,7 +13,9 @@ export function NewGroup() {
         <Icon />
         <Highlight title="Nova turma" subtitle="crie a turma para adicionar as pessoas" />
 
-        <Button title="Criar" />
+        <Input />
+
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   )
