e9df45cb3c2b4b407e5079307417463ada3c2bb8
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2023-04-07 11:19:36 -0300

    Implementing goback button on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { BackButton, BackIcon, Container, Logo } from "./styles";
 import logoImg from '@assets/logo.png'
 
@@ -6,11 +7,18 @@ type HeaderProps = {
 }
 
 export function Header({ showBackButton = false }: HeaderProps) {
+
+  const navigation = useNavigation()
+
+  function handleGoBack() {
+    navigation.navigate('groups')
+  }
+
   return (
     <Container>
       {
         showBackButton &&
-        <BackButton>
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
       }
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { Header } from "@components/Header";
 import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
@@ -5,6 +6,13 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
+
+  const navigation = useNavigation()
+
+  function handleNew() {
+    navigation.navigate("players", { group: 'Jorginho Gameplays' })
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -17,7 +25,11 @@ export function NewGroup() {
           placeholder="Nome da turma"
         />
 
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button
+          title="Criar"
+          style={{ marginTop: 20 }}
+          onPress={handleNew}
+        />
       </Content>
     </Container>
   )
