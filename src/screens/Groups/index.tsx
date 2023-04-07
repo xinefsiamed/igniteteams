5fcab00324b334b54494726d5995bcc1a27dc10d
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2023-04-07 11:08:10 -0300

    Adding type to routes

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,12 +6,18 @@ import { GroupCard } from "@components/GroupCard";
 import { useState } from "react";
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
+import { useNavigation } from '@react-navigation/native';
 
 export function Groups() {
 
   const [groups, setGroups] = useState<string[]>([
   ])
 
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('new')
+  }
 
   return (
     <Container>
@@ -32,7 +38,7 @@ export function Groups() {
         ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
       />
 
-      <Button title='Adicione' />
+      <Button title='Adicione' onPress={handleNewGroup} />
     </Container>
   )
 }
